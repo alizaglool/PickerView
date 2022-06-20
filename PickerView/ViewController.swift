32eedcef6482b283ba63e5fb450e8c6c8cb5aa37
Ali Zaglool <alizaglool90@gmail.com>
2022-06-20 12:33:08 +0200

    Initial Commit

---
+++ PickerView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PickerView
+//
+//  Created by A_Z on 6/20/22.
+//  Copyright © 2022 AliZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
